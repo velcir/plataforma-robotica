5aa9e036ce6c27efc5d6501a8590b3816883143b
Éverton Roberto Auler <evertonrobertoauler@gmail.com>
2016-05-31 18:04:12 -0300

    Adicionando component router

--- interface/src/index.ts
+++ interface/src/index.ts
@@ -3,9 +3,9 @@
 import * as angular from 'angular';
 
 import 'angular-material';
-
+import '@angular/router/angular1/angular_1_router';
 import 'angular-material/angular-material.css';
 
-const ngModule = angular.module('app', ['ngMaterial']);
+const ngModule = angular.module('app', ['ngMaterial', 'ngComponentRouter']);
 
 console.log(ngModule);
