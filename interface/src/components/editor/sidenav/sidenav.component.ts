6164f73c0200a152f3fd23dd7c5dc1afb5f3de6c
Éverton R. Auler <evertonrobertoauler@gmail.com>
2016-07-03 14:44:36 -0300

    Finalizando atualização interface firebase 3

--- interface/src/components/editor/sidenav/sidenav.component.ts
+++ interface/src/components/editor/sidenav/sidenav.component.ts
@@ -1,14 +1,9 @@
-import {Editor} from '../../../common/services/editor.service';
-
 export const sidenav: angular.IComponentOptions = {
   template: require('./sidenav.component.html'),
   controller: SidenavController
 };
 
-SidenavController.$inject = ['Editor'];
+SidenavController.$inject = [];
 
-function SidenavController(editorService: Editor) {
-  const $ctrl = this;
-
-  console.log(editorService, $ctrl);
+function SidenavController() {
 }
