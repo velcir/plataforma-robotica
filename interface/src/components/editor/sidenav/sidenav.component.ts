67a1a3d44f7f7b0ac9a751f59cd310d02303528a
Éverton R. Auler <evertonrobertoauler@gmail.com>
2016-11-02 10:10:27 -0200

    ajustando interface

--- interface/src/components/editor/sidenav/sidenav.component.ts
+++ interface/src/components/editor/sidenav/sidenav.component.ts
@@ -1,9 +1,28 @@
-
 export const sidenav: angular.IComponentOptions = {
   template: require('./sidenav.component.html'),
   controller: SidenavController
 };
 
-function SidenavController() {
+SidenavController.$inject = ['$mdDialog'];
 
+function SidenavController($mdDialog) {
+
+  const $ctrl = this;
+
+  $ctrl.abrirDialog = abrirDialog;
+
+  function abrirDialog($event, image) {
+    $mdDialog.show({
+      template: require('./dialog.html'),
+      controller: function () {
+        this.fechar = () => $mdDialog.hide();
+        this.image = image;
+      },
+      targetEvent: $event,
+      controllerAs: '$ctrl',
+      bindToController: true,
+      clickOutsideToClose: true,
+      escapeToClose: true
+    });
+  }
 }
